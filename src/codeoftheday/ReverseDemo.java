3a6a9e2ee0a3643aab34792c516dd86e5f999391
MohammedMaisoon <mmohammedmaisoon@gmail.com>
2024-03-29 21:12:33 +0400

    Initial commit

---
+++ src/codeoftheday/ReverseDemo.java
@@ -0,0 +1,21 @@
+package codeoftheday;
+public class ReverseDemo {
+    static void reverseEachWordOfString(String inputString) {
+        String[] words = inputString.split(" ");
+        String reverseString = "";
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            String reverseWord = "";
+            for (int j = word.length() - 1; j >= 0; j--) {
+                reverseWord = reverseWord + word.charAt(j);
+            }
+            reverseString = reverseString + reverseWord + " ";
+        }
+        System.out.println(inputString);
+        System.out.println(reverseString);
+        System.out.println("-------------");
+    }
+    public static void main(String[] args) {
+        reverseEachWordOfString("Most Popular Programming Language is JAVA");
+    }
+}
